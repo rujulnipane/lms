3ab3643d162d4880d08fa8b947dfc95ce0ff831f
rujulnipane <rujulnipane2002@gmail.com>
2024-02-19 19:01:16 +0530

    Course Creation Complete

--- models/SectionModel.php
+++ models/SectionModel.php
@@ -1,21 +1,24 @@
 <?php
 include_once("dbModel.php");
+include_once("VideoModel.php");
 class Section{
     private $db;
     private $video;
     public function __construct(){
         $this->db = Database::getInstance();
         $this->db->getConnection();
+        $this->video = new Video();
     }
 
     public function createSection($courseid, $data){
-        // echo $data[0];
-        // echo $data[1];
+       
         $id = $this->db->insertRecord("SECTION",array(
             "course_id"=> $courseid,
             "title"=> $data[0],
             "details" => $data[1]
         ));
-        $this->video->createVideo($id,$data[2]);
+        foreach($data[2] as $video){
+            $this->video->createVideo($id,$video);
+        }
     }
 }
--- models/VideoModel.php
+++ models/VideoModel.php
@@ -1,12 +1,18 @@
 <?php
-
+include_once("dbModel.php");
 class Video{
+    private $db;
     public function __construct(){
+        $this->db = Database::getInstance();
+        $this->db->getConnection();
     }
 
 
-    public function createVideo($sectionid,$data){
-        
+    public function createVideo($sectionid,$video){
+            $this->db->insertRecord("VIDEO",array(
+                "section_id"=> $sectionid,
+                "video_url" => $video,
+            ));
+    }
     
-    }
 }
